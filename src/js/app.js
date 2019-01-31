d8077a9cd8a431a2874c79ef542085e1a8475295
Seke Nikola <sekenikola@gmail.com>
2019-01-31 14:10:56 +0100

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+// TIME AND DATE
+// Set the date we're counting down to
+const countDownDate = new Date("Jan 1, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function () {
+
+    // Get todays date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="countdown"
+    document.getElementById("countdown").innerHTML = ` ${days} days, ${hours} hours,
+        ${minutes} minutes, ${seconds} sesonds`;
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+// ON SCROLL CHANGE NAVBAR COLOR
+
+$(window).scroll(function () {
+    var offset = $(window).scrollTop();
+    console.log(offset);
+    $('.navbar').toggleClass('shrink', offset > 50);
+});
